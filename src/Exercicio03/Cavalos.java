a4fb0eb7ac0faf2bd2c537d98aa22367f300177f
Marcelo <marcelo.souza@dce.ufpb.br>
2019-09-28 17:10:36 +0100

    Finalizando sistema corrida de cavalos, com 3 threads.

--- src/Exercicio03/Cavalos.java
+++ src/Exercicio03/Cavalos.java
@@ -1,5 +1,38 @@
 package Exercicio03;
 
-public class Cavalos {
+import java.util.Random;
 
+import javax.swing.JLabel;
+
+public class Cavalos extends JLabel implements Runnable {
+	private static final long serialVersionUID = 1L;
+	private Thread cavalo = null;
+	private int posicaoX;
+	private int posicaoY;
+
+	// CONSTRUTOR SOBRECARREGADO
+	public Cavalos(String nome, int posicaoX, int posicaoY) {
+		this.posicaoX = posicaoX;
+		this.posicaoY = posicaoY;
+		cavalo = new Thread(this, nome);
+		cavalo.start();
+
+	}
+
+	// MÉTODO RUN() DA INTERFACE RUNNABLE
+	@Override
+	public void run() {
+		posicaoX += new Random().nextInt(3) * 10;
+		this.setLocation(posicaoX, posicaoY);
+
+		if (posicaoX >= 1280)
+			return;
+
+		try {
+			Thread.sleep(new Random().nextInt(5) * 100);
+			run();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
 }
