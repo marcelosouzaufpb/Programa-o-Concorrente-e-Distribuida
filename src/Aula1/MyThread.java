c76908dfa3e7ddbbf9f8e19ad990cbeef6a013d8
Marcelo <marcelo.souza@dce.ufpb.br>
2019-09-28 15:21:13 +0100

    Criando a classe MyThread e seus atributos e reiscrita de alguns metodos.

---
+++ src/Aula1/MyThread.java
@@ -0,0 +1,23 @@
+package Aula1;
+public class MyThread extends Thread {
+
+	// Costrutor 
+	public MyThread(int i) {
+		super("Thread: " + i);
+	}
+
+	// Funcao run para da starte nas threads
+	@Override
+	public void run() {
+		try {
+			while (true) {
+				// Printe dos nomes das threads 
+				System.out.println(getName());
+				// Funcao para dormir as threds por um valor randomico 
+				sleep((long)(Math.random()));
+			}
+		} catch (InterruptedException e) {
+			// Execao esperada 
+		}
+	}
+}
